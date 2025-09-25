9797e1a9c58686d18c0a716bccd7202c5cc23f46
Zachary K. <zkesler2027@gmail.com>
2025-09-25 16:17:07 +0000

    finished 3

--- RoundThingsDriver.java
+++ RoundThingsDriver.java
@@ -1,9 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
 public class RoundThingsDriver {
   public static void main(String[] args) {
     // declare variable for radius, area, circumference, surfaceArea, and volume here
-
+    double radius;
+    double area;
+    double circum;
+    double surfaceArea;
+    double volume;
     // use a Scanner object to get radius value
+    Scanner scan = new Scanner(System.in);
 
+    System.out.println("Give me a length.");
+     radius = scan.nextDouble();
     // call your methods to test your program
 
     
@@ -13,6 +22,21 @@ public class RoundThingsDriver {
 }
 
   // write your RoundThings class here
+  class Roundthings {
 
+public static double calcCircum(double _radius) {
+  double circumf = 2* _radius * Math.PI;
+  return circumf;
+}
+  public static double calcArea( double _radius) {
+    return Math.PI * (_radius * _radius);
+
+  }
+  public static double calcVol( double _radius){
+    return 0.0;
+  }
+
+
+  }
 
   
--- StringProgram.java
+++ StringProgram.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
 public class StringProgram {
   public static void main(String[] args) {
     // Complete a method to take in a String object from Scanner, remove all * symbols,  
     // and concatenate the four strings in reverse order. Print your new String.
+
+    String newString = "";
+
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Give me a string that contains 4 words seperated");
+    String str = scan.nextLine();
+    String tempStr = str;
+
+    int firstStarPos = tempStr.indexOf("*");
+    newString = tempStr.substring(0, firstStarPos) + " " + newString;
+    tempStr = tempStr.substring( firstStarPos+ 1);
+
+    int secondStarPos = tempStr.indexOf("*");
+    newString = tempStr.substring(0, secondStarPos) + " " + newString;; 
+    tempStr = tempStr.substring( secondStarPos+ 1);
+
+    int thirdStarPos = tempStr.indexOf("*");
+    newString = tempStr.substring(0, thirdStarPos) + " " + newString;; 
+    tempStr = tempStr.substring( thirdStarPos+ 1);
+
+
+    newString = tempStr + " " + newString;
+
+    System.out.println(str + " / " + newString);
 
     
   }
